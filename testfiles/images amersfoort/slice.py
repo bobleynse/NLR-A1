149aedb4cb2d3315a25cadd4c5d3671a04bf3c7c
Bob Leijnse <bobleynse@hotmail.com>
2019-01-17 15:59:07 +0100

    added the roadmap data

--- testfiles/images amersfoort/slice.py
+++ testfiles/images amersfoort/slice.py
@@ -6,26 +6,51 @@ from tifffile import imread, imsave
 
 PIL.Image.MAX_IMAGE_PIXELS = 725050001
 
-im =  Image.open('top10nl_wegvlak_amersfoort.tif')
+# roadmap =  Image.open('top10nl_wegvlak_amersfoort.tif')
+# roadmap = np.array(roadmap)
 
-xmin = 2500
+# print(len(roadmap))
+
+xmin = 0
 ymin = 0
 
-xmax = 5000
-ymax = 2500
+xmax = 25000
+ymax = 25000
 
-sliced = np.array(im)
-sliced = sliced[xmin:xmax, ymin:ymax]
-print(sliced)
+# print(roadmap)
 
-for row in range(len(sliced)):
-    # time indication
-    if (row % 25) == 0:
-        print(round(row / (xmax - xmin) * 100), '%')
-    for col in range(len(sliced[row])):
-        if sliced[row][col] == 255:
-            sliced[row][col] = 0
-        else:
-            sliced[row][col] = 255
+# function to write a tif file in a specified region
+def convertToBlack(roadmap, xmin, xmax, ymin, ymax):
 
-imsave('x={}-{}_y={}-{}.tif'.format(xmin,xmax,ymin,ymax), sliced)
+    slicedRoadmap = roadmap[xmin:xmax, ymin:ymax]
+
+    print()
+    for row in range(len(slicedRoadmap)):
+        # time indication
+        if (row % 25) == 0:
+            print(round(row / (xmax - xmin) * 100, 1), '%')
+        for col in range(len(slicedRoadmap[row])):
+            if slicedRoadmap[row][col] == 255:
+                slicedRoadmap[row][col] = 0
+            else:
+                slicedRoadmap[row][col] = 255
+
+    imsave('x={}-{}_y={}-{}.tif'.format(xmin,xmax,ymin,ymax), slicedRoadmap)
+
+# convertToBlack(roadmap, xmin, xmax, ymin, ymax)
+
+# slice the full BlackRoadmap
+
+roadmap =  Image.open('x=0-25000_y=0-25000.tif')
+roadmap = np.array(roadmap)
+
+
+stepsize = 2500
+xtiles = 10
+ytiles = 10
+
+
+for y in range(0, ytiles * stepsize, stepsize):
+    for x in range(0, xtiles * stepsize, stepsize):
+        slicedRoadmap = roadmap[x:(x + stepsize), y:(y + stepsize)]
+        imsave('x={}-{}_y={}-{}.tif'.format(x , (x + stepsize), y , (y + stepsize)), slicedRoadmap)
