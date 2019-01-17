13385a1e2ad9bfd25505b3509bba4aea0a59dd25
Rutger van Woerkom <rutgervanwoerkom@gmail.com>
2019-01-17 14:45:21 +0100

    commented convertToFeatureFiles

--- segmentationfiles/convertToFeatureFiles.py
+++ segmentationfiles/convertToFeatureFiles.py
@@ -3,6 +3,7 @@ from os import listdir
 from PIL import Image
 from datetime import datetime
 
+# Saving path of every image.
 trainInputImagesPath = 'segmentation-dataset/train-input'
 trainOutputImagesPath = 'segmentation-dataset/train-output'
 testInputImagesPath = 'segmentation-dataset/test-input'
@@ -17,6 +18,7 @@ testOutputImagesFiles = listdir(testOutputImagesPath)
 validInputImagesFiles = listdir(validInputImagesPath)
 validOutputImagesFiles = listdir(validOutputImagesPath)
 
+# Checking if number of input and output images match.
 print(str(datetime.now()) + ': trainInputImagesFiles:', len(trainInputImagesFiles))
 print(str(datetime.now()) + ': trainOutputImagesFiles:',  len(trainOutputImagesFiles))
 if(len(trainInputImagesFiles) != len(trainOutputImagesFiles)):
@@ -52,6 +54,7 @@ for i in range(len(validInputImagesFiles)):
 
 print(str(datetime.now()) + ': input and output files check success')
 
+# Older version of writeDataFile:
 def writeDataFileOld(inputImagePath, outputImagePath, inputImageFiles, outputImageFiles, dataFileName):
     dataFile = open(dataFileName, 'w')
     roadPixel = 1
@@ -75,7 +78,9 @@ def writeDataFileOld(inputImagePath, outputImagePath, inputImageFiles, outputIma
         
         if((inputImageXSize != outputImageXSize) or (inputImageYSize != outputImageYSize)):
             raise Exception('train inputImage and outputImage mismatch at index', str(i))
-        
+
+        # This for loop assures that the algorithm only looks at pixel which have enough 
+        # surrounding pixels (avoiding edges).
         for x in range(rectSize//2, inputImageXSize - (rectSize//2)):
             for y in range(rectSize//2, inputImageYSize - (rectSize//2)):
                 isRoadPixel = outputImagePixels[x, y]
@@ -98,6 +103,8 @@ def writeDataFileOld(inputImagePath, outputImagePath, inputImageFiles, outputIma
                 line += str(roadPixel if isRoadPixel else nonroadPixel) + '\n'
                 dataFile.write(line)
     
+# Modify the size of the pixel window in this function.
+# Modify the amound to pixel lines in the .csv file in this function.
 def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageFiles, dataFileName):
     dataFile = open(dataFileName, 'w')
     rectSize = 5
@@ -106,6 +113,7 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
     linesCountPerImage = 0
     linesLimitPerImage = (linesLimit / len(inputImageFiles)) + 1
     
+    # Looping through all input files.
     for i in range(len(inputImageFiles)):
         print(str(datetime.now()) + ': prcessing image', i)
         linesCountPerImage = 0
@@ -113,6 +121,7 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
         inputImageXSize, inputImageYSize = inputImage.size
         # inputImagePixels = inputImage.load()
         
+        # Selecting output image file.
         outputImage = Image.open(outputImagePath + '/' + outputImageFiles[i])
         outputImageXSize, outputImageYSize = outputImage.size
         outputImagePixels = outputImage.load()
@@ -120,9 +129,14 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
         if((inputImageXSize != outputImageXSize) or (inputImageYSize != outputImageYSize)):
             raise Exception('train inputImage and outputImage mismatch at index', str(i))
 
+        # Creating the pixel arrays in which roads and non-roads are collected.
         outputImageRoadPixelsArr = []
         outputImageNonRoadPixelsArr= []
         
+        # This for loop assures that the algorithm only looks at pixel which have enough 
+        # surrounding pixels (avoiding edges).
+        # If there is a road in the output image, the pixel values of the input image are
+        # appended to the corresponding array.
         for x in range(rectSize//2, inputImageXSize - (rectSize//2)):
             for y in range(rectSize//2, inputImageYSize - (rectSize//2)):
                 isRoadPixel = outputImagePixels[x, y]
@@ -131,9 +145,11 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
                 else:
                     outputImageNonRoadPixelsArr.append((x, y))
 
+        # The pixel arrays are randomly shuffled, to get a random distribution of all the scanned images.
         random.shuffle(outputImageRoadPixelsArr)
         random.shuffle(outputImageNonRoadPixelsArr)
         
+
         for m in range(len(outputImageRoadPixelsArr)):
             if(linesCountPerImage >= linesLimitPerImage):
                 break
@@ -141,9 +157,11 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
             if(((m*2) + 1) >= len(outputImageNonRoadPixelsArr)):
                 break
             
+            # Select x and y coordinates from the output pixel arrays.
             x = outputImageRoadPixelsArr[m][0]
             y = outputImageRoadPixelsArr[m][1]
             
+            # Cropping the input image according to rectSize to create a 'sub image'.
             rect = (x - (rectSize//2), y - (rectSize//2), x + (rectSize//2) + 1, y + (rectSize//2) + 1)
             subImage = inputImage.crop(rect).load()
             line = ''
@@ -153,15 +171,18 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
                     line += str(subImage[i, j][1]) + ','
                     line += str(subImage[i, j][2]) + ','
             
+            # Going to next line.
             line += str(1) + '\n'
             linesCount += 1
             linesCountPerImage += 1
             dataFile.write(line)
             
+            # Non-road pixels are written in the .csv here.
             for n in range(2):
                 x = outputImageNonRoadPixelsArr[(m*2) + n][0]
                 y = outputImageNonRoadPixelsArr[(m*2) + n][1]
                 
+                # Cropping input image.
                 rect = (x - (rectSize//2), y - (rectSize//2), x + (rectSize//2) + 1, y + (rectSize//2) + 1)
                 subImage = inputImage.crop(rect).load()
                 line = ''
@@ -171,6 +192,7 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
                         line += str(subImage[i, j][1]) + ','
                         line += str(subImage[i, j][2]) + ','
                 
+                # Going to next line.
                 line += str(0) + '\n'
                 linesCount += 1
                 linesCountPerImage += 1
@@ -178,10 +200,12 @@ def writeDataFile(inputImagePath, outputImagePath, inputImageFiles, outputImageF
     
     print(str(datetime.now()) + ': ' + dataFileName + ' linesCount:', linesCount)
 
+# Creating .csv files.
 trainDataFileName = 'segmentation-dataset/train.csv'
 testDataFileName = 'segmentation-dataset/test.csv'
 validDataFileName = 'segmentation-dataset/valid.csv'
 
+# Writing pixel arrays into .csv files.
 print(str(datetime.now()) + ': writing trainDataFile')
 writeDataFile(trainInputImagesPath, trainOutputImagesPath, trainInputImagesFiles, trainOutputImagesFiles, trainDataFileName)
 print(str(datetime.now()) + ': trainDataFile complete')
